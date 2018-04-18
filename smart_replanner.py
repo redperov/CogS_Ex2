22d7b35f1c36d7938f0b79697d383321b0c04430
redperov <redperov@gmail>
2018-04-18 11:02:57 +0300

    Uses plan as long as it is needed

--- smart_replanner.py
+++ smart_replanner.py
@@ -24,46 +24,72 @@ class SmartRePlanner(Executor):
     def next_action(self):
         if self.services.goal_tracking.reached_all_goals():
             return None
-        if len(self.plan) > 0:
-            return self._use_plan()
 
+        # Get all the current valid options.
         options = self.services.valid_actions.get()
+
         if len(options) == 0:
             return None
+
         if len(options) == 1:
+
+            # No need for the current plan.
+            self.plan = []
+            print("Using options")
             return options[0]
 
-        self._plan_k_steps_ahead()
-        return self._use_plan()
+        return self._use_plan(options)
+
+    def _use_plan(self, options):
+        if len(self.plan) == 0:
+            print("Creating new plan")
+            self._create_plan()
+
+        # The next step according to the current plan.
+        plan_step = self.plan.pop(0).lower()
+
+        # Check if the next step according to the plan is valid.
+        if plan_step in options:
+            return plan_step
+
+        # If the plan step is not in options, something changed in the world, re-plan.
+        self.plan = []
+        return self._use_plan(options)
+
+    def _create_plan(self):
+        problem_path = self.services.problem_generator.generate_problem(
+            self.services.goal_tracking.uncompleted_goals[0], self.services.perception.get_state())
+        self.plan = self.services.planner(self.services.pddl.domain_path, problem_path)
+
+        # if len(self.plan) > 0:
+        #     return self._use_plan()
+        #
+        # options = self.services.valid_actions.get()
+        # if len(options) == 0:
+        #     return None
+        # if len(options) == 1:
+        #     return options[0]
+        #
+        # self._plan_k_steps_ahead()
+        # return self._use_plan()
 
         # if len(self.steps) > 0:
         #     return self.steps.pop(0).lower()
         # return None
 
-    def _use_plan(self):
-        if len(self.plan) > 0:
-            return self.plan.pop(0).lower()
-        return None
-
-    def _plan_k_steps_ahead(self):
-        problem_path = self.services.problem_generator.generate_problem(
-            self.services.goal_tracking.uncompleted_goals[0], self.services.perception.get_state())
-        self.plan = self.services.planner(self.services.pddl.domain_path, problem_path)
-
-        print(len(self.plan))
-        # Trim the list to length <= k.
-        self.plan = self.plan[:self.k]
-
-
-
-    # def _pick_from_many(self):
+    # def _use_plan(self):
+    #     if len(self.plan) > 0:
+    #         return self.plan.pop(0).lower()
+    #     return None
+    #
+    # def _plan_k_steps_ahead(self):
     #     problem_path = self.services.problem_generator.generate_problem(
     #         self.services.goal_tracking.uncompleted_goals[0], self.services.perception.get_state())
-    #     plan = self.services.planner(self.services.pddl.domain_path, problem_path)
+    #     self.plan = self.services.planner(self.services.pddl.domain_path, problem_path)
     #
-    #     if len(plan) > 0:
-    #         return plan.pop(0).lower()
-    #     return None
+    #     print(len(self.plan))
+    #     # Trim the list to length <= k.
+    #     self.plan = self.plan[:self.k]
 
 
 if __name__ == "__main__":
